23831e78fe0952c5da8d02948c18542a39a70003
Samuel <samuelh08.shm@gmail.com>
2022-04-24 12:31:59 -0500

    Add log header from requests

--- server/index.js
+++ server/index.js
@@ -19,22 +19,19 @@ app.get('/', (req, res, next) => {
 
 // No route found handler
 app.use((req, res, next) => {
-  const message = 'Route nor found';
-  const statusCode = 404;
-
-  logger.warn(message);
-
-  res.status(statusCode);
-  res.json({
-    message,
+  next({
+    message: 'Route not found',
+    statusCode: 404,
+    level: 'warn',
   });
 });
 
 // Error handler
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
+  const { message, statusCode = 500, level = 'error' } = err;
+  const log = `${logger.header(req)} ${statusCode} ${message}`;
 
-  logger.error(message);
+  logger[level](log);
 
   res.status(statusCode);
   res.json({
