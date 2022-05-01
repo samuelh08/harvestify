df99bfe34fb1679ff8d3fde728a5588390ee7497
Samuel <samuelh08.shm@gmail.com>
2022-05-01 16:49:43 -0500

    Add name and remove password fields for Clients

--- server/api/v1/clients/model.js
+++ server/api/v1/clients/model.js
@@ -16,7 +16,7 @@ const fields = {
     trim: true,
     minLength: 6,
   },
-  name: {
+  firstname: {
     type: String,
     required: true,
     trim: true,
@@ -34,7 +34,36 @@ const fields = {
 
 const client = new Schema(fields, {
   timestamps: true,
+  toJSON: {
+    virtuals: true,
+  },
+  toObject: {
+    virtuals: true,
+  },
 });
+
+client
+  .virtual('name')
+  .get(function getName() {
+    return `${this.firstname} ${this.lastname}`;
+  })
+  .set(function setName(name) {
+    const [firstname = '', lastname = ''] = name.split(' ');
+    this.firstname = firstname;
+    this.lastname = lastname;
+  });
+
+const hiddenFields = ['password'];
+
+client.methods.toJSON = function toJSON() {
+  const doc = this.toObject();
+  hiddenFields.forEach((field) => {
+    if (Object.hasOwnProperty.call(doc, field)) {
+      delete doc[field];
+    }
+  });
+  return doc;
+};
 
 module.exports = {
   Model: mongoose.model('client', client),
