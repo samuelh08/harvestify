88970cf4629d04d858c3f2a037705146437daa1e
JuanE <juanernestogranados@gmail.com>
2022-05-07 09:09:00 -0500

    Added Eslint ignore

--- server/api/v1/auth.js
+++ server/api/v1/auth.js
@@ -5,6 +5,7 @@ const config = require('../../config');
 const { secret, expires } = config.token;
 
 const signToken = (payload, expiresIn = expires) =>
+  // eslint-disable-next-line
   sign(payload, secret, {
     algorithm: 'HS256',
     expiresIn,
