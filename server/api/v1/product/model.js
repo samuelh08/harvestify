7c9094b837d5a4bbb1b87bf2ad93201301a5de08
JuanE <juanernestogranados@gmail.com>
2022-05-07 16:30:50 -0500

    Updated product schema

--- server/api/v1/product/model.js
+++ server/api/v1/product/model.js
@@ -1,17 +1,42 @@
 const mongoose = require('mongoose');
 
-const product = {
-  // _id: mongoose.Schema.Types.ObjectId,  automatically added by mongoose
-  producer_Id: {
-    // eslint-disable-next-line
-    String: mongoose.Schema.Types.ObjectId,
+const { Schema } = mongoose;
+
+const fields = {
+  quantity: {
+    // quantity of product in stock
+    type: String,
+    required: true,
+    trim: true,
+  },
+  unit: {
+    // unit of product [kg, liter, piece, etc]
+    type: String,
+    required: true,
+    trim: true,
+  },
+  name: { String, required: true }, // name of product
+  picture: { String, required: true }, // picture of product
+  category: { String, required: true }, // category of product [vegetable, fruit, etc]
+  price: { Number, required: true }, // price of product by unit
+};
+
+const references = {
+  producerId: {
+    type: Schema.Types.ObjectId,
     ref: 'producer',
     required: true,
   },
-  name: { String, required: true },
-  picture: { String, required: true },
-  category: { String, required: true },
-  price: { Number, required: true },
 };
 
 module.exports = mongoose.model('product', product);
+
+const product = new Schema(fields, {
+  timestamps: true,
+});
+
+module.exports = {
+  Model: mongoose.model('product', product),
+  fields,
+  references,
+};
