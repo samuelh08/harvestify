7deca8f675aa35c14892ffe60b0a0687f13b1ae3
Samuel <samuelh08.shm@gmail.com>
2022-05-29 16:20:42 -0500

    Included producer related routes to index

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -4,10 +4,16 @@ const clients = require('./clients/routes');
 const carts = require('./carts/routes');
 const cartItems = require('./cartItems/routes');
 const reviews = require('./reviews/routes');
+const producers = require('./producers/routes');
+const products = require('./products/routes');
+const locations = require('./locations/routes');
 
 router.use('/clients', clients);
 router.use('/carts', carts);
 router.use('/cartItems', cartItems);
 router.use('/reviews', reviews);
+router.use('/producers', producers);
+router.use('/products', products);
+router.use('/products', locations);
 
 module.exports = router;
